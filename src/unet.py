678bb6b09b85f0dc849fa8007638a8b679d1b84b
ProceduralTree <jonathan.ulmer@ulmail.de>
2026-01-06 15:43:25 +0100

    unet with attention sucess

--- src/unet.py
+++ src/unet.py
@@ -8,8 +8,15 @@ from torch import Tensor
 
 
 class EncoderBlock(nn.Module):
-    def __init__(self, in_ch, out_ch, kernel_size=3, actv=nn.SiLU()):
+    def __init__(
+        self, in_ch, out_ch, embedding_dimension, kernel_size=3, actv=nn.SiLU()
+    ):
         super().__init__()
+        self.time_embedding = nn.Sequential(
+            nn.Linear(embedding_dimension, in_ch * 2),
+            nn.SiLU(),
+            nn.Linear(in_ch * 2, in_ch * 2),
+        )
         self.conv = nn.Sequential(
             nn.Conv2d(
                 in_ch,
@@ -29,9 +36,11 @@ class EncoderBlock(nn.Module):
         )
         self.pool = nn.MaxPool2d(2)
 
-    def forward(self, x):
-        x = self.conv(x)
+    def forward(self, x, t):
         skip = x
+        gamma, mu = self.time_embedding(t).chunk(2, dim=1)
+        x = x * (1 + gamma[:, :, None, None]) + mu[:, :, None, None]
+        x = self.conv(x)
         x = self.pool(x)
         return x, skip
 
@@ -42,11 +51,17 @@ class DecoderBlock(nn.Module):
         in_ch,
         skip_ch,
         out_ch,
+        embedding_dimension,
         kernel_size=3,
         actv=nn.SiLU(),
     ):
         super().__init__()
         self.up = nn.ConvTranspose2d(in_ch, in_ch, kernel_size=2, stride=2)
+        self.time_embedding = nn.Sequential(
+            nn.Linear(embedding_dimension, (in_ch + skip_ch) * 2),
+            nn.SiLU(),
+            nn.Linear((in_ch + skip_ch) * 2, (in_ch + skip_ch) * 2),
+        )
         self.conv = nn.Sequential(
             nn.Conv2d(
                 in_ch + skip_ch,
@@ -65,23 +80,33 @@ class DecoderBlock(nn.Module):
             ),
         )
 
-    def forward(self, x, skip):
+    def forward(self, x, skip, t):
+        gamma, mu = self.time_embedding(t).chunk(2, dim=1)
         x = self.up(x)  # upsample
         x = pt.cat([x, skip], 1)  # concatenate skip connection
-        x = self.conv(x)  # refine
+        x = self.conv(
+            x * (1 + gamma[:, :, None, None]) + mu[:, :, None, None]
+        )  # refine
         return x
 
 
 class UNET(nn.Module):
 
     def __init__(
-        self, in_ch, out_ch, depth, kernel_size, input_shape, hidden_factor=10
+        self,
+        in_ch,
+        out_ch,
+        depth,
+        kernel_size,
+        input_shape,
+        hidden_factor=10,
+        embedding_dimension=10,
     ) -> None:
         super().__init__()
         self.depth = depth
         self.hidden_factor = hidden_factor
         self.shape = input_shape
-        self.time_embedding = SinusoidalEmbedding(10, 10, hidden_factor)
+        self.time_embedding = SinusoidalEmbedding(10, 10, embedding_dimension)
         self.depth = depth
         self.encoder = nn.ModuleList()
         self.decoder = nn.ModuleList()
@@ -90,15 +115,25 @@ class UNET(nn.Module):
             nn.SiLU(),
             nn.Conv2d(hidden_factor, out_ch, 3, padding=1, padding_mode="zeros"),
         )
-        self.encoder.append(EncoderBlock(in_ch, hidden_factor))
-        self.decoder.append(DecoderBlock(hidden_factor, hidden_factor, hidden_factor))
+        self.encoder.append(EncoderBlock(in_ch, hidden_factor, embedding_dimension))
+        self.decoder.append(
+            DecoderBlock(hidden_factor, in_ch, hidden_factor, embedding_dimension)
+        )
         for i in range(1, depth):
-            input = hidden_factor
-            output = hidden_factor
-            self.encoder.append(EncoderBlock(input, input, kernel_size))
-            self.decoder.append(DecoderBlock(output, input, output, kernel_size))
+            input = hidden_factor * i
+            output = hidden_factor * (i + 1)
+            self.encoder.append(
+                EncoderBlock(input, output, embedding_dimension, kernel_size)
+            )
+            self.decoder.append(
+                DecoderBlock(output, input, input, embedding_dimension, kernel_size)
+            )
 
-        self.bottleneck = MultiheadAttention(hidden_factor, hidden_factor, 8)
+        self.bottleneck = nn.Sequential(
+            MultiheadAttention(hidden_factor * depth, hidden_factor * depth, 16),
+            nn.SiLU(),
+            MultiheadAttention(hidden_factor * depth, hidden_factor * depth, 16),
+        )
 
     def init(
         self,
@@ -112,16 +147,16 @@ class UNET(nn.Module):
 
     def forward(self, x, t):
         skips = []
-        t_embedd = self.time_embedding(t)[:, :, None, None]
+        t_embedd = self.time_embedding(t)
         for i in range(self.depth):
-            x, skip = self.encoder[i](x)
+            x, skip = self.encoder[i](x, t_embedd)
             skips.append(skip)
         # x_linear: Tensor = x.flatten(start_dim=1)
 
-        x: Tensor = self.bottleneck(x + t_embedd)
+        # x: Tensor = self.bottleneck(x)
         # x = x_linear.reshape(-1, self.hidden_factor, self.shape[0] // 2**self.depth, self.shape[1] // 2**self.depth,)
 
         for i in reversed(range(self.depth)):
             skip = skips.pop()
-            x = self.decoder[i](x, skip + t_embedd)
+            x = self.decoder[i](x, skip, t_embedd)
         return self.post_smoothing(x)
